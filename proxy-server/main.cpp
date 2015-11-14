932540edec9e5ce6172f9e94b24a40ca85fc93b7
Dmitry Novik <d1mas1k4@yandex.ru>
2015-11-14 23:40:39 +0300

    Initial commit

---
+++ proxy-server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proxy-server
+//
+//  Created by Дмитрий Новик on 14.11.15.
+//  Copyright © 2015 Дмитрий Новик. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
